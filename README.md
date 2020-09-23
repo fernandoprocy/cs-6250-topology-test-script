515b026e54d3f6a56c2759fcb7b05810313b135c
fernandoprocy <fernandoprocy@gmail.com>
2020-09-22 22:33:11 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cs-6250-topology-test-script
-Simple Pytest script to test the topologies
+Simple Pytest script to test the topologies for Project 4 of the CS6250 OMSCS Fall 20 class.
+
+# How to use
+
+Disclaimer: Only tested on the VM provided. Python 2.7.6 and pytest 2.5.1 are already installed on the VM.
+
+Clone/Download the repository and extract into the project's root folder.
+
+Run `py.test test_topologies.py` to run the tests with the default settings.
+
+# Customizing
+
+- If your reference files folder isn't named `reference`, you can optionally use an environment variable to set it.
+e.g.: If your reference logs are in a folder called `results`, run `REF_DIR=results py.test test_topologies.py`.
+
+- If you want to skip/add some tests, currently you will have to do it by editing the file itself. Edit the file and comment/add the lines inside the @pytest.mark.parametrize decorator corresponding to the tests you want to skip or add.
---
+++ test_topologies.py
@@ -0,0 +1,83 @@
+import pytest
+import os
+from subprocess import check_call, CalledProcessError, STDOUT
+
+CURRENT_DIR = os.getcwd()
+FNULL = open(os.devnull, 'w')
+REFERENCE_DIR = os.getenv("REF_DIR", "reference")
+REF_LOG_FILE_EXT = ".log"
+RESULT_LOG_FILE_EXT = ".log"
+
+def parse_line(line):
+    # Receive raw line and return tuple (node, set(distance_vector))
+    node, vectors = line.split(":")
+    return (node, set(vectors.split(",")))
+
+def read_file(filename_path):
+    with open(os.path.join(CURRENT_DIR, filename_path), "r") as f:
+
+        # Read the last output fragment only
+        try:
+            log = f.read().split("-----\n")[-2].splitlines()
+        except: # Empty log file, probably from bad topologies
+            log = []
+
+    # Return list of (node, set(distance_vector))
+    return [parse_line(line) for line in log]
+
+def execute_algorithm(topo_name):
+    try:
+        check_call(["./run.sh", topo_name], stdout=FNULL, stderr=STDOUT)
+    except CalledProcessError:
+        raise RuntimeError("Execution of algorithm on topology {} failed.".format(topo_name))
+
+
+@pytest.mark.parametrize("topo_name", [
+    # "BadTopo",  # Warning: It should always be []
+    "SimpleTopo",
+    "SimpleNegativeCycleTopo",
+    "ComplexTopo",
+    "SingleLoopTopo",
+    # "LargeRandom",  # Warning: a bit heavy
+    # "new_ComplexTopoHalfed",
+    # "new_LongLoop",
+    # "new_NegCycleWithBiNegLink",
+    # "new_NoNegTopo",
+    # "new_TailNegativeCycle",
+    # "new_TailNegCycleWithBiNegLink",
+    # "new_v2_SingleNode",
+    # "new_v2_TwoNodesNeg",
+    # "new_v2_TwoNodes",
+    # "new_v2_TwoNodesUni",
+    # "new_v2_VeryComplex2",
+    # "new_v2_VeryComplex",
+    # "new_YoutubeTopo",
+    # "n_longAlternatingSeries",
+    # "n_longNegSeries2",
+    # "n_longNegSeries",
+    # "n_longSeries",
+    # "NodeDetectEarly",
+    # "NodeDetectWithCycle",
+    # "NoInOrOutLinks",
+    # "SimpleNegativeCycle",
+    # "SimpleOddLengthNegativeCycle",
+    # "TwoNegCycles"
+])
+def test_topology(topo_name):
+
+    ## Setup phase
+    execute_algorithm(topo_name)
+
+    # Load reference and result logs
+    ref_log = read_file(os.path.join(REFERENCE_DIR, topo_name + REF_LOG_FILE_EXT))
+    result_log = read_file(topo_name + RESULT_LOG_FILE_EXT)
+
+    # Then
+    assert len(ref_log) == len(result_log)
+    for i in range(len(ref_log)):
+        node_ref, vectors_ref = ref_log[i]
+        node_result, vectors_result = result_log[i]
+
+        assert node_ref == node_result
+        assert vectors_ref == vectors_result
+    
